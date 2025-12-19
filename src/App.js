c16c8e2c22358479970b63d9af4ba9a6ad833553
AnushAdk04 <adhikarianush64@gmail.com>
2025-12-19 11:14:11 +0545

    temperature converter added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Bmi from "./Bmi";
 import Login from "./Login";
+import Temperature from "./Temperature";
 
 function App() {
   return (
     <>
     <Login/>
     <Bmi/>
+    <Temperature/>
     </>
   );
 }

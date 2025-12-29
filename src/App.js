1908f6dea5caa7292e2eea7e5288dd41caa117f0
AnushAdk04 <adhikarianush64@gmail.com>
2025-12-29 11:06:03 +0545

    routing implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import Bmi from "./Bmi";
-import Joke from "./Joke";
-import Login from "./Login";
-import Temperature from "./Temperature";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./Login"
+import Bmi from "./Bmi"
+import Joke from "./Joke"
+import Temperature from "./Temperature"
+import Header from "./Header"
+import Footer from "./Footer"
 
 function App() {
   return (
     <>
-    <Login/>
-    <Bmi/>
-    <Temperature/>
-    <Joke/>
+
+      <BrowserRouter>
+
+      <Header/>
+
+      <Routes>
+        
+        <Route path="/" element={<Login/>}/>
+        <Route path="/bmi" element={<Bmi/>}/>
+        <Route path="/joke" element={<Joke/>}/>
+        <Route path="/temperature" element={<Temperature/>}/>
+
+      </Routes>
+
+      <Footer/>
+
+      </BrowserRouter>
+
     </>
   );
 }
